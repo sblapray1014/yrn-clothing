bcbd5f3020e115a4b6aa9147d97a40ee3fed5bee
Spencer LaPray <sblapray@yahoo.com>
2020-01-11 10:56:35 -0700

    finished up first half on sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -51,7 +52,7 @@ class SignIn extends Component {
             required
           />
 
-          <input type='submit' value='Submit Form' />
+          <CustomButton type='submit'>Sign In</CustomButton>
         </form>
       </div>
     );
